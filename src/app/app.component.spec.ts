04ecba0f8ef4a946b440266ea40f4e15bc5ec07b
Dario Lumbreras <dlumbreras@24hourfit.com>
2018-05-09 14:11:47 -0700

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
+
+  let fixture;
+  let component;
+  let screen;
+  let button;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -8,20 +16,142 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    screen = fixture.debugElement.query(By.css('.screen'));
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  it('should add two numbers (24 + 6 = 30)', async(() => {
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+    button = fixture.debugElement.query(By.css('.b-2'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('2');
+
+    button = fixture.debugElement.query(By.css('.b-4'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('24');
+
+    button = fixture.debugElement.query(By.css('.b-addition'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('');
+
+    button = fixture.debugElement.query(By.css('.b-6'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('6');
+
+    button = fixture.debugElement.query(By.css('.b-equals'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('30');
   }));
+
+  it('should substract two numbers (50 - 35.5 = 14.5)', async(() => {
+    fixture.detectChanges();
+
+    button = fixture.debugElement.query(By.css('.b-5'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('5');
+
+    button = fixture.debugElement.query(By.css('.b-0'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('50');
+
+    button = fixture.debugElement.query(By.css('.b-substraction'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('');
+
+    button = fixture.debugElement.query(By.css('.b-3'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('3');
+
+    button = fixture.debugElement.query(By.css('.b-5'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('35');
+
+    button = fixture.debugElement.query(By.css('.b-decimal'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('35.');
+
+    button = fixture.debugElement.query(By.css('.b-5'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('35.5');
+
+    button = fixture.debugElement.query(By.css('.b-equals'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('14.5');
+  }));
+
+  it('should multiply two numbers (5 * 9 = 45)', async(() => {
+    fixture.detectChanges();
+
+    button = fixture.debugElement.query(By.css('.b-5'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('5');
+
+    button = fixture.debugElement.query(By.css('.b-multiplication'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('');
+
+    button = fixture.debugElement.query(By.css('.b-9'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('9');
+
+    button = fixture.debugElement.query(By.css('.b-equals'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('45');
+  }));
+
+  it('should divide two numbers (100 / 4 = 25)', async(() => {
+    fixture.detectChanges();
+
+    button = fixture.debugElement.query(By.css('.b-1'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('1');
+
+    button = fixture.debugElement.query(By.css('.b-0'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('10');
+
+    button = fixture.debugElement.query(By.css('.b-0'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('100');
+
+    button = fixture.debugElement.query(By.css('.b-division'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('');
+
+    button = fixture.debugElement.query(By.css('.b-4'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('4');
+
+    button = fixture.debugElement.query(By.css('.b-equals'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(screen.properties.value).toBe('25');
+  }));
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  digits: string[] = [];
+  number1: number = null;
+  number2: number = null;
+  operator: string = null;
+  decimal: boolean = false;
+
+  private value() {
+    return parseFloat(this.digits.join(''));
+  }
+
+  onClickDecimal() {
+    if (!this.decimal) {
+      this.decimal = true;
+      this.digits.push('.');
+    }
+  }
+
+  onClickNumber(number) {
+    this.digits.push(number);
+  }
+
+  onClickOperator(operator) {
+    let value = this.value();
+
+    if (!isNaN(value)) {
+      this.number1 = value;
+    }
+
+    this.digits = [];
+    this.number2 = null;
+    this.operator = operator;
+    this.decimal = false;
+  }
+
+  onClickEquals() {
+    let value = this.value();
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    let number1 = this.number1;
+    let number2 = this.number2 !== null ? this.number2 : value;
+    let result = null;
+
+    switch (this.operator) {
+      case '/': result = number1 / number2; break;
+      case '*': result = number1 * number2; break;
+      case '-': result = number1 - number2; break;
+      case '+': result = number1 + number2; break;
+    }
+
+    if (result === null) {
+      return;
+    }
+
+    this.digits = result.toString().split('');
+    this.number1 = result;
+    this.number2 = number2;
+    this.decimal = false;
+  }
+
+  onClickClear() {
+    this.digits = [];
+    this.number1 = null;
+    this.number2 = null;
+    this.operator = null;
+    this.decimal = false;
+  }
+
 }
